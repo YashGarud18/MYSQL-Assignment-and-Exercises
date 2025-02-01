bda71256f4086e9e56a8a68e5ff241549f55fd66
YashGarud18 <yashgarud18@gmail.com>
2025-02-01 17:13:26 +0530

    Add files via upload

---
+++ Assignment_2.sql
@@ -0,0 +1,11 @@
+-- ---------------------------------------------------------Assignment-2 ----------------------------------------------------
+/*                          |            Implement Healthcare System Using Database from Scratch           |                 
+							-------------------------------------------------------------------------------
+                                             - Name your database Healthcare_System.
+                                             - Design 10 Tables with Attributes:
+                                             - Each table must have at least 5 attributes with appropriate data types.
+                                             - Include 10 records in each table.                                             */
+-- ----------------------------------------------------------------------------------------------------------------------------
+
+-- Creating database
+create database if not exists Healthcare_System;
